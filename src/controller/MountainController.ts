231597ad8d415c058760bbe29414d4a4b8d5036e
Hidehiro Aya <hi.dehiro424@gmail.com>
2020-11-24 08:41:53 +0900

    controllerファイル一部修正

--- src/controller/MountainController.ts
+++ src/controller/MountainController.ts
@@ -1,10 +1,10 @@
 import {getRepository} from "typeorm";
 import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import {Mountain} from "../entity/Mountain";
 
 export class UserController {
 
-    private userRepository = getRepository(User);
+    private userRepository = getRepository(Mountain);
 
     async all(request: Request, response: Response, next: NextFunction) {
         return this.userRepository.find();
@@ -23,3 +23,4 @@ export class UserController {
         await this.userRepository.remove(userToRemove);
     }
 
+}
